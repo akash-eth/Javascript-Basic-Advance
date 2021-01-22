9a3a8136a99c9035e48ad84156da7a77f5b9f166
akashbhardwaj04 <akkiworkspace@gmail.com>
2021-01-22 11:26:10 +0530

    added a signUp trial

---
+++ js-signUp/script.js
@@ -0,0 +1,20 @@
+const uid = "akash123";
+
+var fullName = "Akash Bhardwaj";
+
+var emailId = "akash@mail.com";
+
+var numberOfVisits = 28;
+
+var isLoggedInFromGoogle = false;
+
+
+// One way to take output from user:
+// prompt("user name is", fullName);
+
+// console.log("User name is:", fullName);
+
+console.log(`
+    user name is : ${fullName}
+    emailId is: ${emailId}
+`);
