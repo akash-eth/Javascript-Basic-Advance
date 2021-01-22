0fd6a49dae2ba0e19f365432b45a5745bf868019
akashbhardwaj04 <akkiworkspace@gmail.com>
2021-01-22 10:55:36 +0530

    updated variables

---
+++ js-variables/script.js
@@ -0,0 +1,13 @@
+var fullName = "Akash Bhardwaj";
+
+var courseName = "JS-Basics";
+
+var loggedCount = 34;
+
+var isLoggedIn = true;
+
+var paymentMode;
+
+paymentMode = "Debit Card";
+
+console.log(paymentMode);
