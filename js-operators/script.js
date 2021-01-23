b33ecf608a8f82dc85fc5f86c9459bcd5e1a7757
akashbhardwaj04 <akkiworkspace@gmail.com>
2021-01-23 12:09:57 +0530

    Discount functionality

---
+++ js-operators/script.js
@@ -0,0 +1,12 @@
+// operators available in JS:
+
+// finding discount on an product:
+
+var sellPrice = 800;
+var originalPrice = 1350;
+
+var discountPrice = ((originalPrice - sellPrice)/originalPrice)*100;
+
+console.log(`
+    Discount on the product is: ${discountPrice}
+`);
