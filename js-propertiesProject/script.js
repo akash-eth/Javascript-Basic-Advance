e5a6149bdc70379f4b36a08bb29b5ab6208c415a
akashbhardwaj04 <akkiworkspace@gmail.com>
2021-02-01 15:07:38 +0530

    updated properties

--- js-propertiesProject/script.js
+++ js-propertiesProject/script.js
@@ -13,4 +13,23 @@ var getBackGroundColor = (selectedElement) => {
     return window.getComputedStyle(selectedElement).backgroundColor;
 }
 
-console.log(getBackGroundColor(pink));
+//console.log(getBackGroundColor(pink));
+
+// var color = getBackGroundColor(pink);
+
+
+// pink.addEventListener("mouseenter", () => {
+//     center.style.background = color;
+// })
+
+var magicBackgroundColor = (element, color) => {
+    return element.addEventListener("mouseenter", () => {
+        center.style.background = color;
+        })
+}
+
+console.log(magicBackgroundColor(red, getBackGroundColor(red)));
+console.log(magicBackgroundColor(cyan, getBackGroundColor(cyan)));
+console.log(magicBackgroundColor(violet, getBackGroundColor(violet)));
+console.log(magicBackgroundColor(orange, getBackGroundColor(orange)));
+console.log(magicBackgroundColor(pink, getBackGroundColor(pink)));
