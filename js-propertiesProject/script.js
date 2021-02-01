c2998ec195c9b65bb516fe68368649912c0c972a
akashbhardwaj04 <akkiworkspace@gmail.com>
2021-02-01 14:43:10 +0530

    obtained properties

---
+++ js-propertiesProject/script.js
@@ -0,0 +1,10 @@
+var red = document.querySelector(".red")
+var cyan = document.querySelector(".cyan")
+var violet = document.querySelector(".violet")
+var orange = document.querySelector(".orange")
+var pink = document.querySelector(".pink")
+
+var center = document.querySelector(".center")
+
+
+console.log(window.getComputedStyle(red).backgroundColor);
