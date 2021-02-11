112b6b5f343aae10a0701767e8f22fb06100c1d1
Akash Bhardwaj <akkiworkspace@gmail.com>
2021-02-11 15:22:05 +0530

    testing

--- js-arrays/fillFilter.js
+++ js-arrays/fillFilter.js
@@ -1,12 +1,12 @@
 //fill:
 
 var testArray = [4,3,6,5,9,7,1,6,2];
-//console.log(testArray.fill("empty",2,5));
+console.log(testArray.fill("empty",2,5));
 
 //filter:
 
 var number = [23,45,67,89,43,28,59,61,97];
-// console.log(number);
-// console.log(number.filter((e) => (e%2 === 0)));
+console.log(number);
+console.log(number.filter((e) => (e%2 === 0)));
  var names = ["Akash", "amit", "hargobind", "omkar"];
  console.log(names.filter((e) => (e!= (e.length <=4))));
