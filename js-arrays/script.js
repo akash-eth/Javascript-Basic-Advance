7452f76bae38d594c5804678e8bfdb10874ec1de
Akash Bhardwaj <akkiworkspace@gmail.com>
2021-03-08 11:39:59 +0530

    updated arrays

--- js-arrays/script.js
+++ js-arrays/script.js
@@ -10,13 +10,13 @@ var states = new Array("rajasthan","maharastra","punjab","goa");
 
 
 states.push("assam");
-// console.log(states);
+console.log(states);
 
 states.unshift("haryana");
-//console.log(states);
+console.log(states); // adds new element at the starting of the element
 
 states.shift();
-console.log(states);
+console.log(states); // removes the first element from the array
 
 console.log(states.indexOf("punjab"));
 console.log(states.indexOf("newyy")); // if this does not exist will return -1.
